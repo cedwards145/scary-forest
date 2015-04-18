0a40880ae4632511b577bfd19186a67871b0069d
Cam Edwards <cameronedwards145@gmail.com>
2015-04-18 19:38:04 +0100

    Scripts

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody))]
+[RequireComponent (typeof(Collider))]
+public class Pickup : MonoBehaviour 
+{
+	public float pickupDistance = 3;
+	private GameObject player;
+	private Transform playerWeaponTransform;
+
+	void Awake()
+	{
+		player = GameObject.FindWithTag ("Player");
+		playerWeaponTransform = GameObject.Find("PlayerWeapon").transform;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Transform transform = GetComponent<Transform> ();
+
+		float distance = Vector3.Distance (transform.position, player.transform.position);
+
+		// If player is close enough to pick up item, and presses E
+		if (distance < pickupDistance && Input.GetKeyDown (KeyCode.E))
+		{
+			// Get player's current weapon
+			GameObject playerWeapon = GameObject.Find ("EquippedWeapon");
+
+			// If player has weapon already, drop it
+			if (playerWeapon != null)
+			{
+				playerWeapon.transform.parent = null;
+				playerWeapon.name = "Pickup";
+				playerWeapon.AddComponent<Pickup>();
+				playerWeapon.GetComponents<Rigidbody>()[0].isKinematic = false;;
+			}
+
+			GameObject weapon = this.gameObject; 
+			weapon.GetComponents<Rigidbody>()[0].isKinematic = true;
+			weapon.transform.parent = playerWeaponTransform;
+			weapon.transform.localPosition = Vector3.zero;
+			weapon.transform.localRotation = Quaternion.identity;
+			weapon.transform.localScale = Vector3.one;
+			weapon.name = "EquippedWeapon";
+			Destroy (this);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float throwStrength = 500;
+	private GameObject playerCamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Awake()
+	{
+		playerCamera = GameObject.Find ("PlayerCamera");
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (Input.GetMouseButtonDown (1)) 
+		{
+			GameObject weapon = GameObject.Find ("EquippedWeapon");
+			if (weapon != null)
+			{
+				Rigidbody body = weapon.GetComponent<Rigidbody> ();
+				dropWeapon(weapon);
+				body.AddForce (playerCamera.transform.forward * throwStrength);
+			}
+		}
+	}
+
+	public void dropWeapon(GameObject weapon)
+	{
+		dropWeapon (weapon, null);
+	}
+
+	public void dropWeapon(GameObject weapon, Rigidbody body)
+	{
+		if (body == null)
+			body = weapon.GetComponent<Rigidbody> ();
+
+		weapon.transform.parent = null;
+		weapon.name = "Pickup";
+		weapon.AddComponent<Pickup>();
+		body.isKinematic = false;
+	}
+}
